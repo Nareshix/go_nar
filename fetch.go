f6c4a28837c936e2aeea468681f0f5df838a0f15
nareshix <nareshix66@gmail.com>
2025-02-25 18:27:57 +0800

    generate link

---
+++ fetch.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net/http"
+	"strings"
+
+	"github.com/tidwall/gjson"
+)
+
+func Fetch(app string) any {
+	url := "https://raw.githubusercontent.com/Nareshix/repos/refs/heads/main/apps.json"
+
+
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// app.download
+	var downloadKey strings.Builder 
+	downloadKey.WriteString(app)
+	downloadKey.WriteString(".download")
+
+	downloadLink := gjson.GetBytes(body, downloadKey.String())
+
+
+
+	if downloadLink.Exists(){
+		return &downloadLink
+	}else{
+		return "no link"	
+	}
+}
---
+++ main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/urfave/cli/v2"
+)
+
+func main() {
+	app := &cli.App{
+		Commands: []*cli.Command{
+			{
+				Name:    "install",
+				Aliases: []string{"i"},
+				Usage:   "install app",
+				Action: func(cCtx *cli.Context) error {
+					// fmt.Println("added task: ", cCtx.Args().First())
+					if cCtx.Args().Len() == 0 {
+						fmt.Println("nar install <app>")
+						return nil
+					}else{
+						downloadLink :=  Fetch(cCtx.Args().First())
+						fmt.Println(downloadLink)
+					}
+
+					return nil
+				},
+			},
+			{
+				Name:    "complete",
+				Aliases: []string{"c"},
+				Usage:   "complete a task on the list",
+				Action: func(cCtx *cli.Context) error {
+					fmt.Println("completed task: ", cCtx.Args().First())
+					return nil
+				},
+			},
+			{
+				Name:    "template",
+				Aliases: []string{"t"},
+				Usage:   "options for task templates",
+				Subcommands: []*cli.Command{
+					{
+						Name:  "add",
+						Usage: "add a new template",
+						Action: func(cCtx *cli.Context) error {
+							fmt.Println("new task template: ", cCtx.Args().First())
+							return nil
+						},
+					},
+					{
+						Name:  "remove",
+						Usage: "remove an existing template",
+						Action: func(cCtx *cli.Context) error {
+							fmt.Println("removed task template: ", cCtx.Args().First())
+							return nil
+						},
+					},
+				},
+			},
+		},
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
+}
