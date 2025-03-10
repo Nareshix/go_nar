55239217869dbab331dfd247e7e98f863b9bfec3
nareshix <nareshix66@gmail.com>
2025-03-10 16:45:15 +0800

    full path is added within code, thus db does not need full link but instead the name

--- download.go
+++ download.go
@@ -11,84 +11,83 @@ import (
 
 // checks for download script --> deb --> tar.gz
 
-func Download(downloadLink string, binPath string, symlink string,deleteCmd string,autoDownload bool) {
-	
+func Download(downloadLink string, binPath string, symlink string, deleteCmd string, autoDownload bool) {
+
 	// If autodownload script is avail use that only
-	if autoDownload{
+	if autoDownload {
 		fmt.Println(downloadLink)
 		cmd := exec.Command(os.Getenv("SHELL"), "-c", downloadLink)
 		cmd.Stdout = os.Stdout
 		cmd.Stderr = os.Stderr
 		cmd.Stdin = os.Stdin // Required if `wget` asks for input
-		
+
 		if err := cmd.Run(); err != nil {
 			fmt.Println("Unable to run autodownload script")
 			return
 		}
-			return //exit program 
+		return //exit program
 	}
 
 	// grabs the tar.gz or .deb file from github via http
-	cmd := exec.Command("sudo", "wget","-q", "--show-progress", downloadLink)
+	cmd := exec.Command("sudo", "wget", "-q", "--show-progress", downloadLink)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	cmd.Stdin = os.Stdin // Required if `wget` asks for input
-	
+
 	if err := cmd.Run(); err != nil {
 		fmt.Println("Unable to download http link")
 		return
 	}
 
-	parsedDownloadLink,err := url.Parse(downloadLink)
-	if err != nil{
+	parsedDownloadLink, err := url.Parse(downloadLink)
+	if err != nil {
 		fmt.Println("Unable to parse download link")
 		return
 	}
 	fileNameWithExtension := path.Base(parsedDownloadLink.Path)
 
 	// Donwload DEB file
-	if strings.HasSuffix(fileNameWithExtension, ".deb"){
+	if strings.HasSuffix(fileNameWithExtension, ".deb") {
 
 		debFileName := "./" + fileNameWithExtension
-		cmd := exec.Command("sudo", "apt", "install", debFileName )
+		cmd := exec.Command("sudo", "apt", "install", debFileName)
 		cmd.Stdout = os.Stdout
 		cmd.Stderr = os.Stderr
 		cmd.Stdin = os.Stdin // Required if `wget` asks for input
-		
-		if err := cmd.Run(); err != nil{
+
+		if err := cmd.Run(); err != nil {
 			fmt.Println("Unable to install DEB file")
 			return
 		}
-		
+
 		// deleted the .deb file (not needed)
-		if err := os.Remove(fileNameWithExtension); err != nil{
+		if err := os.Remove(fileNameWithExtension); err != nil {
 			fmt.Println("Unable to remove DEB file")
 			return
 		}
-		
-	// download tar binaries in /opt/ and create a symlink to /usr/local/bin
-	} else if strings.HasSuffix(fileNameWithExtension, ".tar.gz"){
-		
-		cmd1 := exec.Command("sudo", "tar", "-xvzf", fileNameWithExtension,"-C", "/opt")
-		if err := cmd1.Run(); err != nil{
+
+		// download tar binaries in /opt/ and create a symlink to /usr/local/bin
+	} else if strings.HasSuffix(fileNameWithExtension, ".tar.gz") {
+
+		cmd1 := exec.Command("sudo", "tar", "-xvzf", fileNameWithExtension, "-C", "/opt")
+		if err := cmd1.Run(); err != nil {
 			fmt.Println("failed to untar file")
 		}
-		
+
 		// remove the tar.gz file (not needed)
-		if err := os.Remove(fileNameWithExtension); err != nil{
+		if err := os.Remove(fileNameWithExtension); err != nil {
 			fmt.Println("Unable to remove tar.gz. file")
 			return
 		}
-		
+
 		fullPathToBin := "/opt/" + binPath
-		cmd2 := exec.Command("sudo", "ln", "-s", fullPathToBin, symlink )
-		if err := cmd2.Run(); err != nil{
+		fullPathToSymlink := "/usr/local/bin/" + symlink
+		cmd2 := exec.Command("sudo", "ln", "-s", fullPathToBin, fullPathToSymlink)
+		if err := cmd2.Run(); err != nil {
 			fmt.Println("failed to create symlink")
 			return
 
-		
-	} 
-
+		}
 
 	}
 }
